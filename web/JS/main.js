40ebad21dfe401477c840525d9775c8be77cade8
Steve Moss <smoss@catalystitservices.com>
2015-07-27 14:12:52 -0700

    Section 3 compelted.

--- web/JS/main.js
+++ web/JS/main.js
@@ -5,8 +5,10 @@
  */
 
 var app = angular.module('myApp', []);
+var apiKey = 'MDE5OTkxMjkyMDE0MzgwMTg3MzBiOWEyOA001',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-app.controller('PlayerController', ['$scope', function($scope) {
+app.controller('PlayerController', ['$scope', function($scope, $http) {
   $scope.playing = false;
   $scope.audio = document.createElement('audio');
   $scope.audio.src = '/media/npr.mp4';
@@ -14,6 +16,7 @@ app.controller('PlayerController', ['$scope', function($scope) {
     $scope.audio.play();
     $scope.playing = true;
   };
+    
   $scope.stop = function() {
     $scope.audio.pause();
     $scope.playing = false;
@@ -22,6 +25,17 @@ app.controller('PlayerController', ['$scope', function($scope) {
     $scope.$apply(function() {
       $scope.stop();
     });
+  });
+  
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+      
+      
+      $scope.programs = data.list.story;
+  }).error(function(data, status) {
+    //error message here
   });
 }]);
 
